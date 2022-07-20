0ce098d2f7df68a0e9fb67a7c80b704f5e171e3f
ABOOBA <spanky@yandex.ru>
2022-07-20 10:21:03 +0300

    - fix service - add new lib version

--- java_pro/hw02-generics/src/main/java/homework/CustomerService.java
+++ java_pro/hw02-generics/src/main/java/homework/CustomerService.java
@@ -5,19 +5,20 @@ import java.util.*;
 public class CustomerService {
 
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    private final TreeMap<homework.Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    private final TreeMap<homework.Customer, String> map =
+            new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<homework.Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
         return Optional.ofNullable(map.firstEntry())
-                .map(e -> e == null ? e : new AbstractMap.SimpleImmutableEntry
+                .map(e -> new AbstractMap.SimpleImmutableEntry<>
                         (new Customer(e.getKey().getId(), e.getKey().getName(), e.getKey().getScores()),
                                 e.getValue())).orElse(null);
     }
 
-    public Map.Entry<homework.Customer, String> getNext(homework.Customer customer) {
+    public Map.Entry<homework.Customer, String> getNext(Customer customer) {
         return Optional.ofNullable(map.higherEntry(customer))
-                .map(e -> e == null ? e : new AbstractMap.SimpleImmutableEntry
+                .map(e -> new AbstractMap.SimpleImmutableEntry<>
                         (new Customer(e.getKey().getId(), e.getKey().getName(), e.getKey().getScores()),
                                 e.getValue())).orElse(null);
     }
